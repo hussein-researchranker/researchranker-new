359819ef65a09832642c65e928efab723f329860
hussein-researchranker <husseinjk40@gmail.com>
2026-08-27 03:00:18 +0300

    security: protect search page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,5 +1,7 @@
+import { auth } from "@clerk/nextjs/server";
 import SearchWizard from "@/components/search/SearchWizard";
 
-export default function SearchPage() {
+export default async function SearchPage() {
+  await auth.protect();
   return <SearchWizard />;
 }
